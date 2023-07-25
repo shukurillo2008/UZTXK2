c8bd7ddc9b0a3341e9d497592a63cdab6de6c8a1
Obd2004 <obidjonabd1097@gmail.com>
2023-07-25 18:09:50 +0500

    list tugamadi

--- dashboard/boss/urls.py
+++ dashboard/boss/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('index/boss', views.index_boss, name='index_boss_url'),
     path('detail/<int:pk>', views.worker_detail, name='detail_url'),
+    path('section/list', views.section_list, name='section_list_url'),
+    path('section/create', views.create_section_user, name='section_detail_url'),
+    path('section/create', views.create_section_user, name='create_section_url'),
+    path('section/update/<int:id>', views.section_update, name='section_update_url'),
+    path('section/delate/<int:id>', views.section_delate, name='section_delate_url'),
 ]
--- dashboard/boss/views.py
+++ dashboard/boss/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from main import models
 from datetime import datetime
+from django.http import HttpResponse
 
 def index_boss(request):
     staff = models.Worker.objects.all().count()
@@ -8,8 +9,7 @@ def index_boss(request):
     not_in_work = models.Worker.objects.filter(in_work=False)
     not_in_work_count = staff - enter
 
-    tuday = datetime.now()
-    kitchen = models.Kitchen.objects.get(date=tuday)
+    kitchen = models.Kitchen.objects.last()
     in_kitchen = models.InKitchen.objects.filter(kitchen = kitchen).count()
     context = {
         'workers_count': staff,
@@ -37,3 +37,78 @@ def worker_detail(request, pk):
     return render(request, 'boss/worker_detail.html', context)
 
 
+def section_list(request):
+    sections_count = models.Section.objects.all().count()
+    ctsDic = {}
+    for cts in models.Section.objects.all():
+        ctsQnt = models.Worker.objects.filter(section=cts).count()
+        ctsDic[cts] = ctsQnt
+
+    context = {
+        'sections':ctsDic,
+
+
+    }
+    return render(request, 'boss/section_list.html', context)
+
+def section_detail(request,id):
+    pass
+
+
+def create_section_user(request):
+    if request.user.is_superuser == True:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            title = request.POST.get('title')
+            try:
+                user_staff = models.User.objects.create_user(
+                    first_name = first_name,
+                    last_name = last_name,
+                    username=username,
+                    password=password,
+                )
+                models.Section.objects.create(
+                    boss = user_staff,
+                    title = title
+                )
+                return redirect('create_section_url')
+            except: 
+                return HttpResponse('Some thing went wrong =(')
+        else:
+            return render(request, 'boss/section_create.html')
+    else:
+        return HttpResponse('You are not super admin')
+    
+
+def section_update(request, id):
+    section = models.Section.objects.get(id=id)
+    if request.user.is_superuser == True or section.boss == request.user:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            title = request.POST.get('title')
+            section.boss.first_name = first_name
+            section.boss.last_name = last_name
+            section.boss.username = username
+            section.boss.password = password
+            section.title = title
+            section.save()
+            return render(request, 'section_update.html')
+        else:
+            return render(request, 'section_detail.html',  context={'section': section})
+    else:
+        return HttpResponse('You have not access!')
+
+
+def section_delate(request, id):
+    section_delate = models.Section.objects.get(id=id)
+    if request.user.is_superuser == True or section_delate.boss == request.user:
+        section_delate.delete()
+    else:
+        return HttpResponse('You have not access!')
+    return redirect('list_sections')
